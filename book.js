f88774568d6d0ed4df136e6eae6afe16a5fbecfa
Adedapo <adedapoagbolaha@gmail.com>
2023-11-24 06:41:05 -0800

    first commit

---
+++ book.js
@@ -0,0 +1,13 @@
+// let button = document.querySelectorAll(".btnsn");
+// button.forEach(function (btn) {
+//   btn.addEventListener("click", function (e) {
+//     e.preventDefault();
+    
+//     Swal.fire({
+//       title: "Good job!",
+//       text: "You clicked the button!",
+//       icon: "success"
+//     });
+//   }); 
+
+// })
